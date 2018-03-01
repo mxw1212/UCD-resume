b88b4cfa6c0eaf2bfd97f62c189b454147bc229f
Matthew Wheeler <mxw1212@gmail.com>
2018-03-01 18:10:41 +0000

    js file updates with my lat and lng

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,28 +2,29 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 55.3781,
+            lng: 3.4360
                 }
     });
             
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             
     var locations = [
-        {lat: 40.785091, lng: -73.968285 },
-        {lat: 41.084045, lng: -73.874245 },
-        {lat: 40.754932, lng: -73.984016 }
+        {lat: 53.378051, lng: -1.435973 },
+        {lat: 53.800755, lng: -1.549077 },
+        {lat: 53.699729, lng: -1.782501 }
+        
     ];
+    
                 
     var markers = locations.map(function(location, i) {
-        return new google.maps.map.Marker({
+        return new google.maps.Marker({
             position: location,
                 label: labels[i % labels.length]
             });
     });
             
-    var markerCluster = new MarkerClusterer(map, markers,
-        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
       }
 
  
